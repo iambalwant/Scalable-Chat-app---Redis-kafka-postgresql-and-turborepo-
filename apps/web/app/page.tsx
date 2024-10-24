342520bebf180a3a095a08997f59122b2fc0bf36
iambalwant <balwantverma55555@gmail.com>
2024-10-25 00:14:43 +0530

    Socket complete ~ no DB

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,17 +5,21 @@ import classes from './page.module.css'
 
 export default function Page(){
 
-  const { sendMessage } = useSocket();
+  const { sendMessage, messages } = useSocket();
   const [message, setMessage] = useState("");
 
   return(
     <div>
-      <div>
-        <h1>All message will appear here</h1>
-      </div>
+      
       <div>
         <input onChange={(e) => setMessage(e.target.value)} className={classes["chat-input"]} placeholder="message..." />
         <button onClick={(e) => sendMessage(message)} className={classes["button"]}>Send</button>
+      </div>
+      <div>
+        
+        {messages.map((e:any)=>(
+          <li>{e}</li>
+        ))}
       </div>
     </div>
   )
